bb8068d1f64ea5933555d019cb1e688a219f44c5
NickolasMoreira <nickolas.tato@gmail.com>
2020-03-26 18:47:34 -0300

    EXercicio DAO, TO, Service e Factory

---
+++ Pais DAO,TO,Service e Factory/Paises/src/Service/PaisService.java
@@ -0,0 +1,40 @@
+package Service;
+
+import Modelo.Pais;
+import DAO.PaisesDAO;
+
+public class PaisService{
+	
+	PaisesDAO dao = new PaisesDAO();
+	
+	public int criar(Pais paises) {
+		return dao.criar(paises);
+	}
+	
+	public void atualizar(Pais paises) {
+		 dao.atualizar(paises);
+	}
+	
+	public void excluir(int id){
+		dao.excluir(id);
+	}
+	
+	public Pais carregar(int id){
+		 return dao.carregar(id);
+	}
+	
+	public void menorArea(Pais paises) {
+		dao.menorArea(paises);
+	}
+	
+	public long maiorPopulacao(long populacao) {
+		return dao.maiorPopulacao(populacao);
+	}
+	
+	
+	public Pais[] tresPaises() {
+		return dao.tresPaises();
+		
+	}
+
+}
